ebab03f5ded1803db342e3c273d4c758f4e456b5
gzdgul <gzdgul00@gmail.com>
2023-03-10 16:47:40 +0300

    improve bootstrap

--- index.js
+++ index.js
@@ -4,19 +4,18 @@ const result_element = document.getElementById('result');
 const usernameInput = document.getElementById('username');
 
 const posssibleChoices = document.querySelectorAll('.options');
-let userChoice
-let computerChoice
 let result
 let username
 
+const main_div = document.getElementById('main');
 const button_div = document.getElementById('button_div');
 const options_div = document.getElementById('options_div');
 
 let nextButton_element = document.getElementById('next_button');
 nextButton_element.addEventListener('click', () => {
-    if (usernameInput.value){
+    if (usernameInput.value.trim()){
         username = usernameInput.value;
-        button_div.className = null;
+        button_div.className = null; // d-none remover. because we want show them.
         console.log(username);
     }
     else {
@@ -32,10 +31,11 @@ posssibleChoices.forEach((possibleChoice) => {
         if (usernameInput.value) {
             let userChoice_id = a.target.id // seçilenin id değerini alır ex> id: rock_id ;
             let userChoice_button = document.getElementById(userChoice_id);
-            userChoice = userChoice_element.innerText = userChoice_button.innerText;
-            computerChoice = computerChoice_element.innerText = generateComputerChoice();
-            result_element.innerText = getResult();
+            let userChoice = userChoice_element.innerText = userChoice_button.innerText;
+            let computerChoice = computerChoice_element.innerText = generateComputerChoice();
+            result = result_element.innerText = getResult(userChoice, computerChoice);
             options_div.className = null;
+            lookAtResult(result);
         }
         else {
             alert('enter your username');
@@ -61,29 +61,42 @@ function generateComputerChoice() {
     return randomNumber;
 }
 
-function getResult() {
+function getResult(
+    computerChoice,
+    userChoice
+) {
     if (computerChoice === userChoice) {
-        result = 'its a draw!';
+        return 'its a draw!';
     }
     else if (userChoice === 'rock' && computerChoice === 'paper') {
-        result = 'computer wins';
+        return 'computer wins';
     }
     else if (userChoice === 'rock' && computerChoice === 'scissors') {
-        result = username + ' wins';
+        return username + ' wins';
     }
     else if (userChoice === 'paper' && computerChoice === 'rock') {
-        result =  username + ' wins';
+        return username + ' wins';
     }
     else if (userChoice === 'paper' && computerChoice === 'scissors') {
-        result = 'computer wins';
+        return 'computer wins';
     }
     else if (userChoice === 'scissors' && computerChoice === 'rock') {
-        result = 'computer wins';
+        return 'computer wins';
     }
     else if (userChoice === 'scissors' && computerChoice === 'paper') {
-        result =  username + ' wins';
+        return  username + ' wins';
     }
-    return result;
 }
 
-
+function lookAtResult(
+    result
+) {
+    console.log(result);
+    if (result.includes('computer')) {
+        main_div.className = 'bg-danger';
+    }
+    else if (result.includes('draw')) {
+        main_div.className = 'bg-warning';
+    }
+    else  main_div.className = 'bg-success';
+}
